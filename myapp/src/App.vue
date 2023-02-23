b41ab388c5f32b25db5189df71ff9414a917a710
duck_ach <djagmlfk@naver.com>
2023-02-23 21:40:07 +0900

    for-v

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  <!-- Vue를 이용했을 때 장점 : HTML도 반복문으로 쓸수있다. -->
+  <div class="menu">
+    <!--
+      <태그 v-for="작명 in 반복횟수" :key="작명">
+    -->
+    <a v-for="home in menu" :key="home">{{home}}</a>
+
+    <!--
+      data() 안에 요소를 넣어놓으면 v-for문에서 그 요소의 개수만큼 반복문을 돌릴수있다.
+      작명한 변수는 데이터 안의 자료가 된다.
+
+      - key의 용도는 반복문 돌린 요소를 컴퓨터가 구분하기 위해서 사용한다.
+      - in 왼쪽에 작명을 2개까지 할 수 있다. ex (home, police)
+    -->
+  </div>
+
+    <div class="menu">
+    <a v-for="(a,i) in 5" :key="i">{{i}}</a>
+
+    <!--
+      - in 왼쪽에 작명을 2개까지 할 수 있다. ex (home, police)
+    -->
+  </div>
   <div>
       {{logo}} <!--자주 변경되지 않는 데이터들은 Binding 할 일이 없다.-->
       <h4 class="red" :style="스타일">{{products[0]}}</h4> <!--속성에는 {{}} 대신 속성 앞에 :(콜론) 붙여주면 적용이 된다.-->
@@ -13,6 +36,15 @@
       <h4>{{products[2]}}</h4>
       <p>{{ price3 }} 만원</p> <!-- {{}} 중괄호 2개에 data()안에있는 변수명을 적어주기만 하면된다.-->
   </div>
+<!--반복문 과제-->
+<h3>반복문과제</h3>
+<div v-for="(products,prices) in products" :key="products">
+  <div>
+      {{logo}} <!--자주 변경되지 않는 데이터들은 Binding 할 일이 없다.-->
+      <h4 class="red" :style="스타일">{{products}}</h4> <!--속성에는 {{}} 대신 속성 앞에 :(콜론) 붙여주면 적용이 된다.-->
+      <p>{{prices}} 만원</p>
+  </div>
+</div>
 </template>
 
 <script>
@@ -38,7 +70,9 @@ export default {
       price3 : 65,
       logo : '원룸샵',
       스타일 : 'color : blue',
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      prices : [80, 50, 60],
+      menu : ['Home', 'Shop', 'About'],
     }
   },
   components: {
@@ -53,6 +87,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding:10px;
+}
+
 </style>
