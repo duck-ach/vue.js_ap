7f004ebf3231f6d7784364495dee77010762fd7c
duck_ach <djagmlfk@naver.com>
2023-03-01 23:02:27 +0900

    모달창 생성, 닫기버튼

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,4 +1,15 @@
 <template>
+
+  <!-- 모달창 UI --> <!-- v-if는 Ture일때만 UI를 보여줄 수 있음 -->
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니 = false" class="btn_modal_close">닫기</button>
+    </div>
+  </div>
+
+
 <!-- run : npm run serve -->
   <!-- Vue를 이용했을 때 장점 : HTML도 반복문으로 쓸수있다. -->
   <div class="menu">
@@ -16,7 +27,7 @@
     -->
   </div>
 
-    <div class="menu">
+  <div class="menu">
     <a v-for="(a,i) in 5" :key="i">{{i}}</a>
 
     <!--
@@ -50,23 +61,29 @@
 <!--이벤트 핸들러로 허위매물 버튼 만들기-->
 <h3>허위매물버튼</h3>
 <div>
-    <h4>{{products[0]}}</h4> <!--Vanila js에서는 버튼누르면 숫자찾아서 +1, 그리고 그걸 HTML에 반영 -->
+    <img class="room-img" src="./assets/room0.jpg">
+    <h4 @click="모달창열렸니 = true">{{products[0]}}</h4> <!--Vanila js에서는 버튼누르면 숫자찾아서 +1, 그리고 그걸 HTML에 반영 -->
     <p>50만원</p> <!-- click 이벤트에 사용하는 것 v-on: = @ --> <!--신고수를 Data Binding--> <!-- vue는 실시간 렌더링이되므로..! -->
     <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
 </div> <!-- '신고수++' 대신 '+=신고수' 사용해도 됨 --> <!-- @mouseover, @drag, @input(input Tag일때), ... 등등 다양한 이벤트가 있음 -->
 <div>
-    <h4>{{products[1]}}</h4>
+    <img class="room-img" src="./assets/room1.jpg">
+    <h4 @click="모달창열렸니 = true">{{products[1]}}</h4>
     <p>60만원</p> <!-- vue에서는 함수를 호출할 때 ()를 붙이지 않는다. -->
     <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
 </div>
 <div>
-    <h4>{{products[2]}}</h4>
+    <img class="room-img" src="./assets/room2.jpg">
+    <h4 @click="모달창열렸니 = true">{{products[2]}}</h4>
     <p>70만원</p>
     <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
 </div>
 </template>
 
 <script>
+
+
+
 
 
 
@@ -84,6 +101,7 @@ export default {
   name: 'App',
   data() { // data(){return{}} 안이 data 통 과 같음 여기에 vue 코드를 모두 짜면 된다.
     return {
+      모달창열렸니 : false, // true는 열렸다. false는 닫혔다. 
       price1 : 80, // key : value 형태로 저장해주면 된다. (변수 : 값할당)
       price2 : 70,
       price3 : 65,
@@ -111,6 +129,39 @@ export default {
 </script>
 
 <style>
+/* 모달창 */
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.btn_modal_close {
+  width : 10%;
+  border: none;
+  font-weight: 700;
+}
+
+/* image */
+.room-img {
+  width : 300px;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -119,6 +170,7 @@ export default {
   color: #2c3e50;
 }
 
+/* nevigation bar */
 .menu {
   background: darkslateblue;
   padding: 15px;
