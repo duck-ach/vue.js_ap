3aa86ca92fd737bd8f05390ba820b1c9951e9aa7
duck_ach <djagmlfk@naver.com>
2024-05-01 18:06:08 +0900

    modal창 및 내부 데이터 바인딩

--- 쀼우/vuedongsan/src/App.vue
+++ 쀼우/vuedongsan/src/App.vue
@@ -7,8 +7,19 @@
     <!--Vue 반복문 특 변수 작명 2개까지 가능 왼쪽변수:array 내 데이터, 오른쪽변수:1씩 증가하는 정수-->
   </div>
 
-  <div v-for="(product, i) in products" :key="i">
-      <h4>{{product}}</h4>
+
+  <div v-for="(product, i) in products" :key="i" class="section">
+    <!-- v-if="조건식" -->
+      <div class="black-bg" v-if="모달창열렸니[i] == true">
+        <div class="white-bg">
+          <h4>{{product}} 상세페이지임</h4>
+          <p>{{prices[i]}} 만원임</p><br/>
+          <button @click="모달창열렸니[i] = false">닫기</button>
+        </div>
+      </div>
+
+      <img :src=images[i] class="profile_img">
+      <h4 @click="모달창열렸니[i] = true">{{product}}</h4>
       <p>{{prices[i]}} 만원</p>
       <!-- v-on:click="자바스크립트" -->
       <button @click="신고수s[i] += 1">허위매물신고</button> <span>신고수 : {{신고수s[i]}}</span>
@@ -22,6 +33,8 @@ export default {
   name: 'App',
   data() {
     return {
+      모달창열렸니 : [false, false, false],
+      images : ['	http://localhost:8080/img/dog.d475cc24.jpg', "http://localhost:8080/img/IMG_5536.4ad8e957.jpg", "	http://localhost:8080/img/IMG_6049.7849fcd9.jpg"],
       신고수s : [0, 0, 0],
       // object 자료형식 모든 데이터 넣을 수 있음. ex) 변수
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
@@ -42,6 +55,25 @@ export default {
 
 <!--CSS-->
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+  left: 0%;
+  top: 0%;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+  margin-top: 200px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -57,8 +89,16 @@ export default {
   border-radius: 5px;
 }
 .menu a {
-
   color: white;
   padding: 10px;
+}
+
+.section {
+  padding: 30px;
+}
+
+.profile_img {
+  width: 100px;
+  height: 100px;
 }
 </style>
